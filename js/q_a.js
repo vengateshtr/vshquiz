ba1032503bab6ae8820f6128ecee4dd05b4e8f8f
vengatesh tr <tr.vengatesh@gmail.com>
2021-07-25 17:45:18 +0530

    core script - detect correct and incorrect is working Next :- display points

--- js/q_a.js
+++ js/q_a.js
@@ -1,32 +1,53 @@
-window['Question_Answers'] = [
+window['Questions_with_Options'] = [
 	{
 		'question': 'what is your favourite color ?',
 
 		'options': [
-			'red',
-			'green',
-			'blue',
-			'pink'
+			{
+				'label': 'red',
+				'point': 0
+			},
+			{
+				'label': 'green',
+				'point': 0
+			},
+			{
+				'label': 'blue',
+				'point': 0
+			},
+			{
+				'label': 'pink',
+				'point': 1
+			}
 		],
 
-		'answers': [
-			'3'
-		]
+		'type': "radiobutton"
 	},
 
 	{
 		'question': 'what is your favourite food ?',
 
 		'options': [
-			'chappathi',
-			'dosa',
-			'biriyani',
-			'idly'
+			{
+				'label': 'chappathi',
+				'point': 0
+			},
+			{
+				'label': 'dosa',
+				"point": 1
+			},
+			{
+				'label': 'biriyani',
+				'point': 0
+			},
+			{
+				'label': 'idly',
+				'point': 1
+			}
 		],
 
-		'answers': [
-			'1', '3'
-		]
+		'type': "checkbox"
+
 	}
 
 ];
--- js/quiz_main.js
+++ js/quiz_main.js
@@ -29,23 +29,160 @@ function quiz_controller(
 
 	function configureScopeValues(){
 
-		vmQC.Question_Answers = window['Question_Answers'];
+		vmQC.Questions_with_Options = window['Questions_with_Options'];
+		vmQC.userSelectedOptions = {};
 
 		vmQC.Callbacks = {};
-		vmQC.Callbacks.verifyAnswers = verifyAnswers;
+		vmQC.Callbacks.onTickRadioButton = onTickRadioButton;
+		vmQC.Callbacks.onTickCheckBox = onTickCheckBox;
+		vmQC.Callbacks.verifyOptions = verifyOptions;
+
+		vmQC.Questions_with_Options.forEach(
+			function(questionItem, questionIndex, questionArray){
+
+				if(
+					questionItem.type === 'radiobutton'
+				){
+
+					questionArray[questionIndex].selectedOptions = [];
+
+				}
+				else if(
+					questionItem.type === 'checkbox'
+				){
+
+					questionArray[questionIndex].selectedOptions = {};
+
+				}
+
+			}
+		);
 
 	}
 
-	function verifyAnswers(){
+	function verifyOptions(){
 
-		debugger;
+		var totalPoints = 0,
+			optionPoint,
+			checkBoxQuestionAllPoints,
+			checkBoxQuestionUserPoints;
 
-		vmQC.Question_Answers;
+		vmQC.Questions_with_Options.forEach(
+			function(questionItem, questionIndex, questionArray){
+
+				var userSelectedOptions;
+
+				if(
+					questionItem.type === 'radiobutton'
+				){
+					userSelectedOptions = vmQC.userSelectedOptions[questionIndex] || [];
+
+					if(
+						userSelectedOptions.length > 0
+					){
+
+						optionPoint = questionItem.options[userSelectedOptions[0]].point;
+
+						if(
+							optionPoint > 0
+						){
+							questionArray[questionIndex].options[userSelectedOptions[0]].userPickedOption_correct = true;
+						}
+						else{
+							questionArray[questionIndex].options[userSelectedOptions[0]].userPickedOption_incorrect = true;
+						}
+
+
+						totalPoints += optionPoint;
+					}
+
+				}
+				else if(
+					questionItem.type === 'checkbox'
+				){
+
+					checkBoxQuestionAllPoints = 0;
+					checkBoxQuestionUserPoints = 0;
+
+					userSelectedOptions = vmQC.userSelectedOptions[questionIndex] || [];
+
+
+					questionItem.options.forEach(
+						function(QOItem, QOIndex, QOArray){
+
+							checkBoxQuestionAllPoints += QOItem.point;
+
+					});
+
+					userSelectedOptions.forEach(
+						function(USOItem, USOIndex, USOArray){
+
+							optionPoint = questionItem.options[USOItem].point;
+
+							if(
+								optionPoint > 0
+							){
+
+								questionArray[questionIndex].options[USOItem].userPickedOption_correct = true;
+
+							}
+							else{
+								questionArray[questionIndex].options[USOItem].userPickedOption_incorrect = true;
+							}
+
+							checkBoxQuestionUserPoints += optionPoint;
+
+						}
+					);
+
+						// AND condition -> only if all options are selected, given mark. Otherwise, ZERO only
+					if(
+						checkBoxQuestionAllPoints === checkBoxQuestionUserPoints
+					){
+						totalPoints += 1;
+					}
+
+				}
+
+			}
+		);
+
+
 
 	}
 
+	function onTickRadioButton(questionIndex, optionIndex){
 
+		vmQC.userSelectedOptions = vmQC.userSelectedOptions || {};
 
+		vmQC.userSelectedOptions[questionIndex] = [
+			optionIndex.toString()
+		];
+
+	}
+
+	function onTickCheckBox(questionIndex, optionIndex, that){
+
+		var unselectedOptionIndex;
+
+		vmQC.userSelectedOptions = vmQC.userSelectedOptions || {};
+
+		vmQC.userSelectedOptions[questionIndex] = vmQC.userSelectedOptions[questionIndex] || [];
+
+		if(
+			vmQC.Questions_with_Options[questionIndex].selectedOptions[optionIndex]
+		){
+
+			unselectedOptionIndex = vmQC.userSelectedOptions[questionIndex].indexOf(optionIndex.toString());
+
+			vmQC.userSelectedOptions[questionIndex].splice(unselectedOptionIndex, 1);
+		}
+		else{
+			vmQC.userSelectedOptions[questionIndex].push(optionIndex.toString());
+
+		}
+
+	}
 
 
 }
